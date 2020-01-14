a0bf90a8c07cba0464371e960f1ebda2060040ba
Robert Collins <rpc219@gmail.com>
2020-01-14 00:02:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,3 @@
-array = ["win", "I", "will", "the", "game"]
-
 def using_include(array, element)
   array.include?(element)
 end
@@ -23,10 +21,3 @@ end
 def using_size(array)
   array.size
 end
-
-print using_include(array, "hope")
-print using_sort(array)
-print using_reverse(array)
-print using_first(array)
-print using_last(array)
-print using_size(array)
